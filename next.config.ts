01f8d9e4d2c00a92e7e4a38dd104ca7718d6d98a
Puneet Sethi <puneet.s@zoftsolutions.com>
2025-09-07 16:36:41 +0530

    dummy comit

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 // next.config.js
 const isProd = process.env.NODE_ENV === 'production';
-const repoName = 'portfolio-ps'; // <-- change this (for project pages)
+const repoName = 'portfolio-ps'; 
 
 const nextConfig: NextConfig = {
   /* config options here */
